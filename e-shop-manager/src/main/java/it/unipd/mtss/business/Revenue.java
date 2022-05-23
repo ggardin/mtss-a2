d2af458c7be654fcbacda6891541f964e5c208ef
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-05-23 13:13:19 +0200

    update Revenue class and close #10

--- e-shop-manager/src/main/java/it/unipd/mtss/business/Revenue.java
+++ e-shop-manager/src/main/java/it/unipd/mtss/business/Revenue.java
@@ -7,15 +7,37 @@ package it.unipd.mtss.business;
 
 import it.unipd.mtss.business.exception.BillException;
 import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.itemType;
 import it.unipd.mtss.model.User;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Revenue implements Bill {
 
   @Override
   public double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException {
-    return 0;
+    if(itemsOrdered == null) {
+      throw new BillException("La lista degli EItems non può essere nulla.");
+    }
+
+    if(itemsOrdered.size() == 0) {
+      throw new BillException("La lista degli EItems non può essere vuota.");
+    }
+
+    if(itemsOrdered.size() > 30) {
+      throw new BillException("Non è possibile eseguire un ordinazione con più di 30 elementi.");
+    }
+
+    double orderPrice = 0.0;
+
+    for (EItem item: itemsOrdered) {
+      if (item == null) {
+        throw new BillException("La lista contiene un oggetto nullo.");
+      }
+      orderPrice += item.getPrice();
+    }
+    return orderPrice;
   }
 }
 
