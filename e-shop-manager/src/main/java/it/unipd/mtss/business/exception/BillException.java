c8673359274edd1050b5ce1a1e108a9817d4cc76
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-05-23 12:00:07 +0200

    create BillException.java (#4)

---
+++ e-shop-manager/src/main/java/it/unipd/mtss/business/exception/BillException.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// [NICHOLAS] [PILOTTO] [1230237]
+// [GIOVANNI] [GARDIN] [2010003]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss.business.exception;
+
+public class BillException extends RuntimeException {
+
+    public BillException(String error) {
+        super(error);
+    }
+}
